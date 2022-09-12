5faec1e16991ef19e986e144e1a056a1a149e0bd
chizoba <chizoba.victory1@gmail.com>
2022-09-12 08:02:04 +0100

    My mini challenge assignment answers

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,18 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    let result = []
+    for(let i = 0; i < array[0].length; i++){
+        let col = []
+
+        for(let j = 0; j < array.length; j++){
+            col.push(array[j][i])
+        }
+        result.push(col)
+    }
+    return result
+}
+
 
 module.exports = transpose;
